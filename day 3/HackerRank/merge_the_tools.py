e418d59309e102867260b6f04dd922b40e3f460f
Annshiv <annamalaipalani11@gmail.com>
2020-09-22 23:54:51 +0530

    Retried merge the tools problem in hackerRank

--- day 3/HackerRank/merge_the_tools.py
+++ day 3/HackerRank/merge_the_tools.py
@@ -1,7 +1,4 @@
-string = 'AABCAAADA'
-k = 3
-lst = []
-start = int(len(string) / k)
-for i in range(start,len(string)+1,k):
-    lst.append(string[:i])
-print(lst)
+S, N = input(), int(input()) 
+for part in zip(*[iter(S)] * N):
+    d = dict()
+    print(''.join([ d.setdefault(c, c) for c in part if c not in d ]))
