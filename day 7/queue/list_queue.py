1ec7187eec26483142cf4549404eaa815baa2b8d
Annshiv <annamalaipalani11@gmail.com>
2020-09-25 02:37:21 +0530

    Queue implementation using list

---
+++ day 7/queue/list_queue.py
@@ -0,0 +1,16 @@
+queue = []
+
+queue.append(1)
+queue.append(2)
+queue.append(3)
+
+print("Initial Queue : ",end=" ")
+print(queue)
+
+#remove elements in queue
+queue.pop(0)
+queue.pop(0)
+queue.pop(0)
+
+print("After removing elements in queue :",end=" ")
+print(queue)
