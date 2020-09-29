495e6ca9d04929c311159513acd3ed2dd2bb944b
Annshiv <annamalaipalani11@gmail.com>
2020-09-29 22:47:36 +0530

    Implementation of singly linked list

--- day 11/insertion.py
+++ day 11/insertion.py
@@ -42,3 +42,26 @@ class linkedlist:
         while (temp): 
             print(temp.data), 
             temp = temp.next
+
+if __name__=='__main__': 
+  
+    # Start with the empty list 
+    llist = LinkedList() 
+  
+    # Insert 6.  So linked list becomes 6->None 
+    llist.append(6) 
+  
+    # Insert 7 at the beginning. So linked list becomes 7->6->None 
+    llist.push(7); 
+  
+    # Insert 1 at the beginning. So linked list becomes 1->7->6->None 
+    llist.push(1); 
+  
+    # Insert 4 at the end. So linked list becomes 1->7->6->4->None 
+    llist.append(4) 
+  
+    # Insert 8, after 7. So linked list becomes 1 -> 7-> 8-> 6-> 4-> None 
+    llist.insertAfter(llist.head.next, 8) 
+  
+    print 'Created linked list is:', 
+    llist.printList() 
