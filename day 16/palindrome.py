9a132d6bb57bc8bdb04755e31421af42b1f3423c
Annshiv <annamalaipalani11@gmail.com>
2020-10-04 23:45:41 +0530

    Function to check if a singly linked list is palindrome

---
+++ day 16/palindrome.py
@@ -0,0 +1,50 @@
+class Node: 
+	def __init__(self,data): 
+		
+		self.data = data 
+		self.ptr = None
+
+def ispalindrome(head): 
+ 
+	slow = head 
+
+	stack = [] 
+	
+	ispalin = True
+
+	while slow != None: 
+		stack.append(slow.data) 
+
+		slow = slow.ptr 
+
+	while head != None: 
+
+		i = stack.pop() 
+		if head.data == i: 
+			ispalin = True
+		else: 
+			ispalin = False
+			break
+		head = head.ptr 
+		
+	return ispalin 
+
+one = Node(1) 
+two = Node(2) 
+three = Node(3) 
+four = Node(4) 
+five = Node(3) 
+six = Node(2) 
+seven = Node(1) 
+
+one.ptr = two 
+two.ptr = three 
+three.ptr = four 
+four.ptr = five 
+five.ptr = six 
+six.ptr = seven 
+seven.ptr = None
+
+result = ispalindrome(one) 
+
+print("isPalindrome:", result) 
