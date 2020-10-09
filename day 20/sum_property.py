6f42e15798656f64c898e02bca570454383f5f17
Annshiv <annamalaipalani11@gmail.com>
2020-10-09 09:01:13 +0530

    check children sum property in binary tree

---
+++ day 20/sum_property.py
@@ -0,0 +1,43 @@
+class newNode: 
+	def __init__(self, data): 
+		self.data = data 
+		self.left = None
+		self.right = None
+
+def isSumProperty(node): 
+	left_data = 0
+	right_data = 0
+
+	if(node == None or (node.left == None and
+						node.right == None)): 
+		return 1
+	else: 
+
+		if(node.left != None): 
+			left_data = node.left.data 
+
+		if(node.right != None): 
+			right_data = node.right.data 
+
+		if((node.data == left_data + right_data) and
+						isSumProperty(node.left) and
+						isSumProperty(node.right)): 
+			return 1
+		else: 
+			return 0
+
+# Driver Code 
+if __name__ == '__main__': 
+	root = newNode(10) 
+	root.left = newNode(8) 
+	root.right = newNode(2) 
+	root.left.left = newNode(3) 
+	root.left.right = newNode(5) 
+	root.right.right = newNode(2) 
+	if(isSumProperty(root)): 
+		print("The given tree satisfies the", 
+					"children sum property ") 
+	else: 
+		print("The given tree does not satisfy", 
+				"the children sum property ") 
+
