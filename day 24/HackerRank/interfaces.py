819771f51c39e29d43a871d5899684771b08b014
Annshiv <annamalaipalani11@gmail.com>
2020-10-15 11:30:57 +0530

    Interfaces problem in hackerrank

---
+++ day 24/HackerRank/interfaces.py
@@ -0,0 +1,7 @@
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        result = 1
+        for i in range(2,n+1):
+            if n % i == 0:
+                result += i
+        return result
