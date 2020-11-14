bf39e684157cc7f96d6c60f07530f4956d919194
Annshiv <annamalaipalani11@gmail.com>
2020-11-14 22:23:46 +0530

    Bank account manager

--- day 44/bank.py
+++ day 44/bank.py
@@ -1,32 +1,19 @@
 class Account:
-    # Define an __init__ constructor method with attributes shared by all accounts:
     def __init__(self,acct_nbr,opening_deposit):
         self.acct_nbr = acct_nbr
         self.balance = opening_deposit
-    
-    # Define a __str__ mehthod to return a recognizable string to any print() command
+        
     def __str__(self):
         return f'${self.balance:.2f}'
     
-    # Define a universal method to accept deposits
     def deposit(self,dep_amt):
         self.balance += dep_amt
-
-    # Define a universal method to handle withdrawals
+        
     def withdraw(self,wd_amt):
         if self.balance >= wd_amt:
             self.balance -= wd_amt
         else:
-            return 'Funds Unavailable'
-
-class Checking(Account):
-    def __init__(self,acct_nbr,opening_deposit):
-        # Run the base class __init__
-        super().__init__(acct_nbr,opening_deposit)
-
-    # Define a __str__ method that returns a string specific to Checking accounts
-    def __str__(self):
-        return f'Checking Account #{self.acct_nbr}\n  Balance: {Account.__str__(self)}'
+            print('Funds Unavailable') 
 
 class Savings(Account):
     def __init__(self,acct_nbr,opening_deposit):
@@ -87,3 +74,26 @@ def make_wd(cust,acct_type,acct_num,wd_amt):
     for acct in cust.accts[acct_type]:
         if acct.acct_nbr == acct_num:
             acct.withdraw(wd_amt)
+
+class Checking(Account):
+    def __init__(self,acct_nbr,opening_deposit):
+        super().__init__(acct_nbr,opening_deposit)
+    
+    def __str__(self):
+        return f'Checking Account #{self.acct_nbr}\n  Balance: {Account.__str__(self)}'
+
+    
+class Savings(Account):
+    def __init__(self,acct_nbr,opening_deposit):
+        super().__init__(acct_nbr,opening_deposit)
+
+    def __str__(self):
+        return f'Savings Account #{self.acct_nbr}\n  Balance: {Account.__str__(self)}'
+
+
+class Business(Account):
+    def __init__(self,acct_nbr,opening_deposit):
+        super().__init__(acct_nbr,opening_deposit)
+
+    def __str__(self):
+        return f'Business Account #{self.acct_nbr}\n  Balance: {Account.__str__(self)}'
