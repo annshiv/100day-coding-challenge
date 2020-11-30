bba499712014a4d4e7b75f3da5057f4fb883b7f5
Annshiv <annamalaipalani11@gmail.com>
2020-11-30 21:02:39 +0530

    exponential search

--- day 56/exponential_search.py
+++ day 56/exponential_search.py
@@ -2,42 +2,25 @@ def binarySearch( arr, l, r, x):
 	if r >= l:
 		mid = int(l + ( r-l ) / 2)
 		
-		# If the element is present at 
-		# the middle itself
 		if arr[mid] == x:
 			return mid
-		
-		# If the element is smaller than mid, 
-		# then it can only be present in the 
-		# left subarray
+
 		if arr[mid] > x:
-			return binarySearch(arr, l, 
-								mid - 1, x)
+			return binarySearch(arr, l,mid - 1, x)
 		
-		# Else he element can only be
-		# present in the right
+
 		return binarySearch(arr, mid + 1, r, x)
-		
-	# We reach here if the element is not present
+
 	return -1
 
-# Returns the position of first
-# occurrence of x in array
 def exponentialSearch(arr, n, x):
-	# IF x is present at first 
-	# location itself
 	if arr[0] == x:
-		return 0
-		
-	# Find range for binary search 
-	# j by repeated doubling
+		return 0		
 	i = 1
 	while i < n and arr[i] <= x:
 		i = i * 2
 	
-	# Call binary search for the found range
-	return binarySearch( arr, i / 2, 
-						min(i, n-1), x)
+	return binarySearch( arr, i / 2, min(i, n-1), x)
 	
 
 # Driver Code
