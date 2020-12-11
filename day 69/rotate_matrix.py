efc7fd564285647eb797af64c0bb97482696c9d8
annshiv <annamalaipalani11@gmail.com>
2020-12-11 23:11:09 +0530

    Rotate a matrix by 90 degree

---
+++ day 69/rotate_matrix.py
@@ -0,0 +1,37 @@
+R = 4
+C = 4 
+def reverseColumns(arr): 
+	for i in range(C): 
+		j = 0
+		k = C-1
+		while j < k: 
+			t = arr[j][i] 
+			arr[j][i] = arr[k][i] 
+			arr[k][i] = t 
+			j += 1
+			k -= 1
+	
+# Function for do transpose of matrix 
+def transpose(arr): 
+	for i in range(R): 
+		for j in range(i, C): 
+			t = arr[i][j] 
+			arr[i][j] = arr[j][i] 
+			arr[j][i] = t 
+
+# Function for print matrix 
+def printMatrix(arr): 
+	for i in range(R): 
+		for j in range(C): 
+			print(str(arr[i][j]), end =" ") 
+		print() 
+
+def rotate90(arr): 
+	transpose(arr) 
+	reverseColumns(arr) 
+arr = [[1, 2, 3, 4], 
+		[5, 6, 7, 8], 
+		[9, 10, 11, 12], 
+		[13, 14, 15, 16] ]
+rotate90(arr)
+printMatrix(arr) 
