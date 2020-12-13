2cc46f5c163866ebc77ab39675ea6a108a85d8c3
annshiv <annamalaipalani11@gmail.com>
2020-12-13 21:55:51 +0530

    Lower and Upper triangular matrix of an array

---
+++ day 72/lower_triangle.py
@@ -0,0 +1,44 @@
+def lower(matrix, row, col): 
+
+	
+	for i in range(0, row): 
+	
+		for j in range(0, col): 
+		
+			if (i < j): 
+			
+				print("0", end = " ")
+			
+			else: 
+				print(matrix[i][j], 
+					end = " " )
+		
+		print(" ")
+	
+def upper(matrix, row, col): 
+
+	for i in range(0, row): 
+	
+		for j in range(0, col): 
+		
+			if (i > j): 
+				print("0", end = " ") 
+			
+			else: 
+				print(matrix[i][j], 
+					end = " " )
+		
+		print(" ") 
+
+# Driver Code 
+matrix = [[1, 2, 3], 
+		[4, 5, 6], 
+		[7, 8, 9]]
+row = 3
+col = 3
+	
+print("Lower triangular matrix: ")
+lower(matrix, row, col)
+	
+print("Upper triangular matrix: ")
+upper(matrix, row, col) 
